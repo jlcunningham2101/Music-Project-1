f6196c180bdb86ebdafe3578430c28c5b52ae56f
Twigster <treshagaye@gmail.com>
2022-01-07 21:18:12 -0500

    added submit button and click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+// click function for the search button, which submits the user input
+var searchButton = document.getElementById("search-btn");
+// console.log(searchButton);
+
+searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    // console.log("click");
+    // console.log(this);
+    var userInput = document.getElementById("input-box").val();
+    console.log(userInput);
+})
+
 var apiKey = ("AIzaSyDl7y_TqRCoUxoKJ8d5CPkotvqL4J94ydU") //dans youtube api
 var apiKey2 = ("94f0bce60423d35a4236cdb67e3e5251") //dans musix api
 
