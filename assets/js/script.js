8746031633ced252b4264b18d51272a779f8317e
Daniel Mendez <soliddan211@gmail.com>
2022-01-10 23:45:13 -0500

    added a DarkMode btn and a video on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,106 +24,82 @@ searchButton.addEventListener("click", function (event) {
     //this is the fetch function for audioDB - search for artist name
     var audioArtistUrl = "https://theaudiodb.com/api/v1/json/" + audioApiKey + "/search.php?s=" + userInput;
     // var audioTrackUrl = "https://theaudiodb.com/api/v1/json/" + audioApiKey + "/searchtrack.php?s=" + userInput + userInput2; // requires 2 inputs
-        
-    // artist search- returns general artist info including name/bio/label/genre/picture/social media/last FM Chart
+
+    // artist search- returns general artist info including name/bio/label/genre
     fetch(audioArtistUrl)
-        .then(function(response) {
-            if(response.ok) {
-                response.json().then(function(data) {
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
                     console.log(data);
-        var artistBucketEl = document.getElementById("artist-bucket");
-        var artistNameEl = document.createElement("p");
-        artistNameEl.textContent = "You searched for: " + data.artists[0].strArtist;
-        artistNameEl.classList.add("is-size-5", "has-text-weight-bold")
-        artistBucketEl.appendChild(artistNameEl);
-        var artistBioEl = document.createElement("p");
-        artistBioEl.textContent = data.artists[0].strBiographyEN;
-        artistBioEl.classList.add("is-size-5", "has-text-weight-medium")
-        artistBucketEl.appendChild(artistBioEl);
-        var artistGenreEl = document.createElement("p");
-        artistGenreEl.textContent = "Genre: " + data.artists[0].strGenre;
-        artistGenreEl.classList.add("is-size-5", "has-text-weight-bold")
-        artistBucketEl.appendChild(artistGenreEl);
-        var artistLabelEl = document.createElement("p");
-        artistLabelEl.textContent = "Label: " + data.artists[0].strLabel;
-        artistLabelEl.classList.add("is-size-5", "has-text-weight-bold")
-        artistBucketEl.appendChild(artistLabelEl);
-        var artistPictureEl = document.createElement("p");
-        artistPictureEl.textContent = "Picture: " + data.artists[0].strBanner;
-        artistPictureEl.classList.add("is-size-5", "has-text-weight-bold")
-        artistBucketEl.appendChild(artistPictureEl);
-        var artistSocialMediaEl = document.createElement("p");
-        artistSocialMediaEl.textContent = "Social Media: " + data.artists[0].strFacebook;
-        artistSocialMediaEl.classlist.add("is-size-5", "has-text-weight-bold")
-        artistBucketEl.appendChild(artistSocialMediaEl);
-        var artistLastFmChartEl = document.createElement("p");
-        artistLastFmChartEl.textContent = "Last FM Chart: " + data.artists[0].strLastFMChart;
-        artistLastFmChartEl.classlist.add("is-size-5", "has-text-weight-bold")
-        artistBucketEl.appendChild(artistLastFmChartEl);
+                    var artistBucketEl = document.getElementById("artist-bucket");
+                    var artistNameEl = document.createElement("p");
+                    artistNameEl.textContent = "You searched for: " + data.artists[0].strArtist;
+                    artistNameEl.classList.add("is-size-5", "has-text-weight-bold")
+                    artistBucketEl.appendChild(artistNameEl);
+                    var artistBioEl = document.createElement("p");
+                    artistBioEl.textContent = data.artists[0].strBiographyEN;
+                    artistBioEl.classList.add("is-size-5", "has-text-weight-medium")
+                    artistBucketEl.appendChild(artistBioEl);
+                    var artistGenreEl = document.createElement("p");
+                    artistGenreEl.textContent = "Genre: " + data.artists[0].strGenre;
+                    artistGenreEl.classList.add("is-size-5", "has-text-weight-bold")
+                    artistBucketEl.appendChild(artistGenreEl);
+                    var artistLabelEl = document.createElement("p");
+                    artistLabelEl.textContent = "Label: " + data.artists[0].strLabel;
+                    artistLabelEl.classList.add("is-size-5", "has-text-weight-bold")
+                    artistBucketEl.appendChild(artistLabelEl);
 
+                    albumCall();
 
-// urls/videos/thumbnails on the page?
-// limit bio to 10 lines & then have a read more link?
-// work on showing album info?
-// reset filter button to clear search
-// padding on page
+                });
 
-
-
-        albumCall();
-
+            }
         });
-
-    }
-});
 
 })
 
-    // album search- returns a list of albums, yr release, review & description & intl sales, artist picture
-var albumCall = function() {
+// album search- returns a list of albums, yr release, review & description & intl sales
+var albumCall = function () {
     var audioAlbumUrl = "https://theaudiodb.com/api/v1/json/" + audioApiKey + "/searchalbum.php?s=" + userInput;
     fetch(audioAlbumUrl)
-        .then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-        var albumBucketEl = document.getElementById("album-bucket");
-        var albumNameEl = document.createElement("p");
-        albumNameEl.textContent = "Album#1: " + data.album[0].strAlbum;
-        albumNameEl.classList.add("is-size-5", "has-text-weight-bold")
-        albumBucketEl.appendChild(albumNameEl);
-        var albumYrEl = document.createElement("p");
-        albumYrEl.textContent = "Released: " + data.album[0].intYearReleased;
-        albumYrEl.classList.add("is-size-5", "has-text-weight-bold")
-        albumBucketEl.appendChild(albumYrEl);
-        var albumDescrEl = document.createElement("p");
-        albumDescrEl.textContent = "Released: " + data.album[0].strDescriptionEN;
-        albumDescrEl.classList.add("is-size-5", "has-text-weight-bold")
-        albumBucketEl.appendChild(albumDescrEl);
-        var albumRevwEl = document.createElement("p");
-        albumRevwEl.textContent = "Released: " + data.album[0].strReview;
-        albumRevwEl.classList.add("is-size-5", "has-text-weight-bold")
-        albumBucketEl.appendChild(albumRevwEl);
-        artistPictureE1.textContent = "Picture: " + data.artists[0].strBanner;
-        artistPictureE1.classList.add("is-size-5", "has-text-weight-bold")
-        artistPictureEl.appendChild(artistPictureEl);
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    var albumBucketEl = document.getElementById("album-bucket");
+                    var albumNameEl = document.createElement("p");
+                    albumNameEl.textContent = "Album#1: " + data.album[0].strAlbum;
+                    albumNameEl.classList.add("is-size-5", "has-text-weight-bold")
+                    albumBucketEl.appendChild(albumNameEl);
+                    var albumYrEl = document.createElement("p");
+                    albumYrEl.textContent = "Released: " + data.album[0].intYearReleased;
+                    albumYrEl.classList.add("is-size-5", "has-text-weight-bold")
+                    albumBucketEl.appendChild(albumYrEl);
+                    var albumDescrEl = document.createElement("p");
+                    albumDescrEl.textContent = "Released: " + data.album[0].strDescriptionEN;
+                    albumDescrEl.classList.add("is-size-5", "has-text-weight-bold")
+                    albumBucketEl.appendChild(albumDescrEl);
+                    var albumRevwEl = document.createElement("p");
+                    albumRevwEl.textContent = "Released: " + data.album[0].strReview;
+                    albumRevwEl.classList.add("is-size-5", "has-text-weight-bold")
+                    albumBucketEl.appendChild(albumRevwEl);
+                })
+            }
 
+            // albumApiUrl - works, search is slow
+            // album data we can use: data.album[i].intSales, intYearReleased, strAlbum(album-name), strArtist, strDescriptionEN (album-info), strReview, strWikipediaID
+            // can we find a way to iterate through this data to pull out the info we want to display? 
 
-        // albumApiUrl - works, search is slow
-        // album data we can use: data.album[i].intSales, intYearReleased, strAlbum(album-name), strArtist, strDescriptionEN (album-info), strReview, strWikipediaID
-        // can we find a way to iterate through this data to pull out the info we want to display? 
+            // else {
+            //     // alert("Error: Artist not found.");   // not allowed to use alerts- modals?
+            // }
+            // })
+            // .catch(function(error) {
+            // // alert("Unable to connect AudioDB") // again, not allowed to use alerts
+            // console.log(error);
+            // }); 
         })
-    } 
-    // else {
-    //     // alert("Error: Artist not found.");   // not allowed to use alerts- modals?
-    // }
-    // })
-    // .catch(function(error) {
-    // // alert("Unable to connect AudioDB") // again, not allowed to use alerts
-    // console.log(error);
-    // }); 
-    }) 
-}; 
+};
 //     //this is the fetch function for audioDB - search for artist and song
 //     var audioApiKey = "523532";
 //     var artistName = userInput; // to be linked to search bar for artist name
@@ -150,64 +126,119 @@ var albumCall = function() {
 
 // function featches from youtube api
 
-function searchMusic(event) { 
+function searchMusic(event) {
     event.preventDefault();
     // console.log("click");
     // console.log(this);
-    
+
     console.log(userInput.value.trim());
     var search = userInput.value.trim()
     var youtubeApiKey = "AIzaSyDl7y_TqRCoUxoKJ8d5CPkotvqL4J94ydU";
-    var youtubeApiUrl= "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" + search + "&key=" + youtubeApiKey;
-   
-        
-        fetch(youtubeApiUrl)
-            .then(function (response) {
-                return response.json()
-            }
-            ).then(function (data) {
-                console.log(data.items);
-                // if (list.indexOf() === -1) {   //took out description from function
-                //     list.push()
-                //     localStorage.setItem("name", JSON.stringify(list));
-
-                //     showLocal()
-                // }
-            }).catch(function (err) {
-                res.json(err)
-            })
-    }
-
-    // var listMusic = function (musicIn) {
-
-    //     var firstS = document.createElement("button")
-    //     firstS.classList = " list-group-item list-group-item-action";
-
-    //     firstS.textContent = musicIn;
-    //     listMusicEl.appendChild(firstS)
-    // } 
-    
-    // document.getElementById("list-Music").addEventListener("click", function (event) {
-
-    //     searchMusic(event.target.textContent);
-
-    // })
-
-    // document.getElementById("search-btn").addEventListener("click", function (event) {
-    //     event.preventDefault();
+    var youtubeApiUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=" + search + "&key=" + youtubeApiKey;
 
 
-    //     var musicIn = document.getElementById("input-box").value;
-    //     document.getElementById("input-box").value = "";
 
-    //     if (musicIn) {
+    fetch(youtubeApiUrl)
+        .then(function (response) {
+            return response.json()
+        }
+        ).then(function (data) {
+            console.log(data.items);
 
-    //         searchMusic(musicIn);
+            // var thumbnailEl = document.getElementById("list-music");
+            // var thumb1El = document.createElement("li");
+            // thumb1El.textContent = "See: " + data[0].snippet.thumbnails.high.
+            // // albumNameEl.classList.add("is-size-5", "has-text-weight-bold")
+            // thumb1El.appendChild(thumbnailEl);
 
-    //     }
-    // });
 
-   
+
+
+            // if (list.indexOf() === -1) {   //took out description from function
+            //     list.push()
+            //     localStorage.setItem("name", JSON.stringify(list));
+
+            //     showLocal()
+            // }
+            // }).catch(function (err) {
+            //     res.json(err)
+        })
+}
+
+// // 2. This code loads the IFrame Player API code asynchronously.
+// var tag = document.createElement('script');
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// // 3. This function creates an <iframe> (and YouTube player)
+// //    after the API code downloads.
+// var player;
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: 'M7lc1UVf-VE',
+//         playerVars: {
+//             'playsinline': 1
+//         },
+//         events: {
+//             'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange
+//         }
+//     });
+// }
+
+// // 4. The API will call this function when the video player is ready.
+// function onPlayerReady(event) {
+//     event.target.playVideo();
+// }
+
+// // 5. The API calls this function when the player's state changes.
+// //    The function indicates that when playing a video (state=1),
+// //    the player should play for six seconds and then stop.
+// var done = false;
+// function onPlayerStateChange(event) {
+//     if (event.data == YT.PlayerState.PLAYING && !done) {
+//         setTimeout(stopVideo, 6000);
+//         done = true;
+//     }
+// }
+// function stopVideo() {
+//     player.stopVideo();
+// }
+
+// // var listMusic = function (musicIn) {
+
+// //     var firstS = document.createElement("button")
+// //     firstS.classList = " list-group-item list-group-item-action";
+
+// //     firstS.textContent = musicIn;
+// //     listMusicEl.appendChild(firstS)
+// // } 
+
+// // document.getElementById("list-Music").addEventListener("click", function (event) {
+
+// //     searchMusic(event.target.textContent);
+
+// // })
+
+// // document.getElementById("search-btn").addEventListener("click", function (event) {
+// //     event.preventDefault();
+
+
+// //     var musicIn = document.getElementById("input-box").value;
+// //     document.getElementById("input-box").value = "";
+
+// //     if (musicIn) {
+
+// //         searchMusic(musicIn);
+
+// //     }
+// // });
+
+
 
 searchButton.addEventListener("click", searchMusic);
 
