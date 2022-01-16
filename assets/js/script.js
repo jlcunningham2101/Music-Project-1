1af7f93591fd34b8cd653af52d37fc83edde1a6e
alaramee <93341641+alaramee@users.noreply.github.com>
2022-01-16 08:49:21 -0500

    scroll button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,6 +206,27 @@ resetBtn.addEventListener("click", function (event) {
   document.getElementById("input-box").value = "";
 });
 
+//The button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button it scrolls to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
+
+
 function myFunction() {
   var element = document.body;
   var toggle = element.classList.toggle("dark-mode");
